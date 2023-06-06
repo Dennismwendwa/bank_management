e25b33fbd397a39e337766e81f24da91defc35ea
Dennismwendwa <dennismusemb2@.com>
2023-06-07 00:47:48 +0300

    my first test passed

--- savingrecord/accounts/tests.py
+++ savingrecord/accounts/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class AccountViewsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    def test_register_view_invalid_password(self):
+        response = self.client.post('/register/', {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'email': 'johndoe@example.com',
+            'password1': '1234567',  # Less than 8 characters
+            'password2': '1234567',
+        }, follow=True)  # Add the follow=True parameter)
+#       print(response.status_code)
+#       print(response.content)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, '/accounts/register/')
+
+        # Assert that the error message is displayed
+        messages = list(response.context['messages'])
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), 'The Password Must be 8 or more charactors!')
--- savingrecord/accounts/urls.py
+++ savingrecord/accounts/urls.py
@@ -4,9 +4,9 @@ from .views import CustomPasswordResetConfirmView #import for class bassed views
 
 urlpatterns = [
     #path("", views.home, name="home"),
-	path("", views.register, name="register"),
-	path("register", views.register, name="register"),
-    path("login", views.login, name="login"),
+#	path("", views.register, name="register"),
+	path("register/", views.register, name="register"),
+        path("login", views.login, name="login"),
 	path("logout", views.logout, name="logout"),
 	path("forgot-password", views.forgot_password, name="forgot_password"),
 	path("reset/<uidb64>/<token>", CustomPasswordResetConfirmView.as_view(),
